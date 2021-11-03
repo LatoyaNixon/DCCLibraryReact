2b19442748121be19a52195a540c4f3c4a6a0f0b
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-11-03 11:45:10 -0400

    Created the name and title component

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import "./App.css"
+import TitleBar from "./TitleBar/TitleBar";
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.books = [
+            { title: "To Die For The People", author: "Toni Morrison" },
+            { title: "The Spook Who Sat By The Door", author: "Sam GreenLee" },
+            { title: "Rabbit", author: "Patricia"}
+        ]
+        this.state = {
+            bookNumber: 0
+        };
+    }
+
+    render(){
+        return(
+         <div className="container-fluid">
+             <TitleBar />
+             <div className="row"> 
+             <div className="col-md-4">
+                 {/*Button here to move to the previous book viewed*/}
+             </div>
+             <div className="col-md-4">
+                 {/*Display book with cover here*/}
+                 <h1>{this.books[this.state.bookNumber].title}</h1>
+                 <h4>{this.books[this.state.bookNumber].author}</h4>
+             </div>
+             <div className="col-md-4">
+                 {/*Button here to move to the previous book viewed*/}
+             </div>
+         </div>   
+         </div>
+        )
+    }
+}
+
+export default App;
